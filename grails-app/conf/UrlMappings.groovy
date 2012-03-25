2865be58bea39bcb5673e0070f0e6797957acf64
Owen Rubel <orubel@gmail.com>
2012-03-24 21:06:45 -0700

    removing bad url mapping

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,19 +7,6 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-		
-        // rundeck api
-        "/rundeckapi/$action"{
-			constraints {
-				controller = "rundeck"
-				parseRequest = true
-				switch(action){
-					case 'getNodes':
-						action = [ POST: "getNodesAsXML" ]
-						break;
-				}
-			}
-        }
 
         "/"(view: "/index")
         "500"(view:'/error')
