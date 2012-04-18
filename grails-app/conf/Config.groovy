b47fbfde35fd4b58ef21e74f781c343769176f22
Owen Rubel <orubel@gmail.com>
2012-04-18 10:11:59 -0700

    fixing the properties file to include from home

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,7 +11,7 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
-grails.config.locations = [ "classpath:config.properties"  ]
+grails.config.locations = [ "file:$HOME/.yana/config.properties"  ]
 
 //grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
