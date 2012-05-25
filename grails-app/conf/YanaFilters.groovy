7b6259c72f2bf2ba0228116d2e7c696dc775ce02
Owen Rubel <orubel@gmail.com>
2012-05-25 08:17:46 -0700

    fixes #127

--- grails-app/conf/YanaFilters.groovy
+++ grails-app/conf/YanaFilters.groovy
@@ -19,10 +19,17 @@ class YanaFilters {
 			}
 		}
 		
-		childnodeFilter(controller:'childNode',action:'edit'){
+		childnodeEditFilter(controller:'childNode',action:'edit'){
 			before = {
 				if(params.parent){ params.parent=Node.get(params.parent.toLong()) }
 				if(params.child){ params.child=Node.get(params.child.toLong()) }
+			}
+		}
+		
+		childnodeSaveFilter(controller:'childNode',action:'save'){
+			before = {
+				if(params.parent){ params.parent=params.parent.id }
+				if(params.child){ params.child=params.child.id }
 			}
 		}
 	}
