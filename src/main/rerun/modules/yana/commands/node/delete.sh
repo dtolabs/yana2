9467dacd6ef699d075607f431321eb67c4a9c663
Alex Honor <alexhonor@gmail.com>
2012-06-17 14:50:40 -0700

    Fixed bug in node -action delete

--- src/main/rerun/modules/yana/commands/node/delete.sh
+++ src/main/rerun/modules/yana/commands/node/delete.sh
@@ -12,9 +12,10 @@
 # Send the delete request 
 #
 
-http_code=$(curl -w "%{http_code}" --silent --fail --request GET \
+http_code=$(curl -w "%{http_code}" --silent --fail \
+    --request DELETE \
     ${URL}/api/node/xml/$ID --cookie ${cookie} -o $response)
-[ "${http_code}" -eq 404 ] && return 0
+[  "${http_code}" -eq 200  -o "${http_code}" -eq 404 ] && return 0
 
 
 # ------------------------------
