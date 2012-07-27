e5c2427bdbe2ea43b20433a87357c47d74b69dab
Greg Schueler <greg.schueler@gmail.com>
2012-07-26 17:51:27 -0700

    Allow project param if session not set

--- grails-app/conf/yana2/ProjectFilters.groovy
+++ grails-app/conf/yana2/ProjectFilters.groovy
@@ -5,11 +5,13 @@ class ProjectFilters {
     def filters = {
         all(controller:'*', controllerExclude: 'project|login', action:'*') {
             before = {
-                if(!session.project){
+                if(!session.project && !params.project){
                     redirect(controller: 'project', action: 'list', params: ['mustChoose': 1])
                     return false
                 }else if(!params.project){
                     params.project=session.project
+                }else if(!session.project){
+                    session.project=params.project
                 }
             }
         }
