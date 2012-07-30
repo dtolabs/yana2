5c6ed950990c8d422387f076e38c05db537d9d25
Alex Honor <alexhonor@gmail.com>
2012-07-29 18:16:06 -0700

    Removed timestamps from NodeValue. Granularity for Node changes is at the Node level not at the attribute value.

--- grails-app/domain/com/dtolabs/NodeValue.groovy
+++ grails-app/domain/com/dtolabs/NodeValue.groovy
@@ -1,15 +1,12 @@
 package com.dtolabs
 
-import java.util.Date;
 
 class NodeValue{
-    static belongsTo = [Node,NodeAttribute]
+    static belongsTo = [Node, NodeAttribute]
 
 	Node node
 	NodeAttribute nodeattribute
 	String value
-	Date dateCreated
-	Date dateModified = new Date()
 
     static mapping = {
         value column: 'nodevalue'
