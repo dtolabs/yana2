064f57666e412c06a1fc478b5c7ae1032eb78246
Alex Honor <alexhonor@gmail.com>
2012-07-30 11:35:22 -0700

    Added a couple more tests

--- test/unit/com/dtolabs/ImportServiceTests.groovy
+++ test/unit/com/dtolabs/ImportServiceTests.groovy
@@ -16,10 +16,9 @@ import org.springframework.core.io.ClassPathResource
 Node, NodeValue, NodeAttribute, ChildNode])
 class ImportServiceTests {
 
-
     /*
-     * No yana root element
-     */
+    * No yana root element
+    */
     def bad_xml1 = """<yoda>
   <attributes>
     <attribute id="hostname" filter="String"/>
@@ -103,28 +102,110 @@ class ImportServiceTests {
   </nodes>
 </yana>
     """
+    /**
+     * Basic model plus a new attribute
+     */
+    def ok_xml1v2 = """<yana>
+ <attributes>
+    <attribute id="arch" filter="String"/>
+  </attributes>
 
+  <nodetypes>
+    <nodetype id="host">
+      <description>host type</description>
+      <image>Node.png</image>
+      <nodeAttributes>
+        <nodeAttribute id="node.arch" attribute="arch" required="true"/>
+      </nodeAttributes>
+    </nodetype>
+  </nodetypes>
+
+</yana>
+    """
     /**
      * Validate a model import file
      */
     void testValidate() {
 
         /**
-         * Verify a trivial example
+         * Check bad input XML
+         */
+        try {
+            service.validate(new StringBufferInputStream(bad_xml1))
+        } catch (ImportServiceException e) {
+            assertNotNull("Invalid XML input did not cause an exception", e)
+        }
+        try {
+            service.validate(new StringBufferInputStream(bad_xml2))
+        } catch (ImportServiceException e) {
+            assertNotNull("Invalid XML input did not cause an exception", e)
+        }
+
+        /**
+         * Verify a trivial good example
          */
         service.validate(new StringBufferInputStream(ok_xml1))
+
         /**
-         * Verify a more extensive model
+         * Verify a more extensive model from a test example
          */
         service.validate(asInputStream("/import/example2.xml"))
 
     }
 
+    /**
+     * Test the population of a model
+     */
+    void testPopulate1() {
+
+        /**
+         * Create a project to store a model
+         */
+        Project project = new Project(name: 'test1', description: 'desc').save()
+        /**
+         * Input does not declare any filters yet so define one
+         */
+        new Filter(project: project, dataType: "String", regex: ".*").save()
+
+        /*
+         * Parse the XML input file and populate the model
+         */
+        def nodes = service.populate(new StringBufferInputStream(ok_xml1), project)
+        assertEquals("incorrect number of nodes: " + nodes.size(), nodes.size(), 1)
+
+        /**
+         * Verify the objects have been populated in the model
+         */
+
+        // Attribute
+        def hostname = Attribute.findByProjectAndName(project, "hostname")
+        assertEquals("Attribute name did not match", hostname.name, "hostname")
+
+        // NodeType
+        def host = NodeType.findByProjectAndName(project, "host")
+        assertEquals("NodeType name did not match", host.name, "host")
+
+        // Node
+        def host1 = Node.findByProjectAndName(project, "host1")
+        assertEquals("Node name did not match", host1.name, "host1")
+
+        /**
+         * Declare another attribute for this type to ensure incremental update
+         */
+        service.populate(new StringBufferInputStream(ok_xml1v2), project)
+
+        def arch = Attribute.findByProjectAndName(project, "arch")
+        assertEquals("Attribute name did not match", arch.name, "arch")
+        def att = NodeAttribute.findByNodetypeAndAttribute(host, arch)
+        assertNotNull("Missing type attribute", att)
+        assertEquals("Attribute was not required but should be", true, att.required)
+
+    }
 
     /**
      * Test the population of a model
      */
-    void testPopulate() {
+    void testPopulate2() {
 
         /**
          * Create a project to store a model
@@ -185,6 +266,8 @@ class ImportServiceTests {
             def zip = ChildNode.findByRelationshipName("zip")
             assertEquals("ChildNode name did not match", zip.relationshipName, "zip")
         }
+
+
     }
 
     /**
