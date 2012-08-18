71df2c0c70c7ed3fb4ead7288fcc7665c84ad8d5
Greg Schueler <greg.schueler@gmail.com>
2012-08-17 17:17:27 -0700

    For dev/test configure tomcat users on run-app

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -4,3 +4,20 @@ eventCompileEnd = {
       fileset(file:"${userHome}/.grails/config.properties")
     }
 }
+
+
+eventConfigureTomcat = {tomcat ->
+  if(grails.util.Environment.current.name == 'development'){
+      tomcat.addUser("admin", "admin")
+      tomcat.addRole("admin", "admin")
+      tomcat.addRole("admin", "yana")
+
+      tomcat.addUser("op1", "op1")
+      tomcat.addRole("op1", "operator")
+      tomcat.addRole("op1", "yana")
+
+      tomcat.addUser("arch1", "arch1")
+      tomcat.addRole("arch1", "architect")
+      tomcat.addRole("arch1", "yana")
+    }
+}
